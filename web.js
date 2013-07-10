36a77ead77aa225b6ad17a084ee41c3afa392867
Ubuntu <ubuntu@ip-172-31-37-238.us-west-2.compute.internal>
2013-07-10 08:13:14 +0000

    Updated web.js for Index12

--- web.js
+++ web.js
@@ -6,13 +6,8 @@ var fs = require('fs');
 
 var prnt = fs.readFileSync('index.html').toString("utf-8");
 
-//var buf = new Buffer(24);
-
-//buf.write(fs.readFileSync('index.html'));
-
 app.get('/', function(request, response) {
   response.send(prnt);
-//response.send(fs.readFileSync('index.html').toString("utf-8");
 });
 
 var port = process.env.PORT || 5000;
